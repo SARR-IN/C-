e105e6bac9f88ae6f8ad6f3b708872f2e155d534
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-09-14 15:51:07 +0530

    Create decimal types.c

---
+++ decimal types.c
@@ -0,0 +1,17 @@
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+
+double radius = 8.71;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
