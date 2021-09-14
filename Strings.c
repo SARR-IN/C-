94ee85e9821358101b4cbb93a90217f9de419a75
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-09-14 15:32:07 +0530

    Create Strings.c

---
+++ Strings.c
@@ -0,0 +1,7 @@
+string firstFriend = "Smith";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
