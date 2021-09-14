2a2114c6a17831fdcd2af1e3499fe6df5096a854
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-09-14 15:29:34 +0530

    Create Declare &Use Variable.c

---
+++ Declare &Use Variable.c
@@ -0,0 +1,9 @@
+string aFriend = "John";
+Console.WriteLine(aFriend);
+
+aFriend = "Kalvin";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello " + aFriend);
+
+Console.WriteLine($"Hello {aFriend}");
