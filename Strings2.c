5b4daa1c41ac875171ec3c00e3f829d78e1665c5
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-09-14 15:33:24 +0530

    Create Strings2.c

---
+++ Strings2.c
@@ -0,0 +1,21 @@
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Grow");
+Console.WriteLine(sayHello);
+
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
